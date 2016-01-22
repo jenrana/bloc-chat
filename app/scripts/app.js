c823dd411fa990dc8c921d3e2cbd905be3ea5470
Jennifer Rana <jennifer.rana@gmail.com>
2016-01-22 14:17:04 -0800

    Create room modal

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -17,12 +17,39 @@ blocChat.config(function($stateProvider, $locationProvider) {
 blocChat.factory('Room', ['$firebaseArray', function($firebaseArray) {
     var firebaseRef = new Firebase("https://brilliant-heat-7733.firebaseio.com/");
     var rooms = $firebaseArray(firebaseRef.child('rooms'));
-
     return {
-        all: rooms
+        all: rooms,
+        create: function(room){
+            return rooms.$add(room)
+        }
     };
 }]);
 
-blocChat.controller('RoomCtrl', function($scope, Room) {
+blocChat.controller('RoomCtrl', function($scope, Room, $uibModal) {
     $scope.rooms = Room.all;
+    $scope.animationsEnabled = true;
+    $scope.open = function () {
+    var modalInstance = $uibModal.open({
+        animation: $scope.animationsEnabled,
+        templateUrl: '/templates/newroom.html',
+        controller: 'ModalCtrl'
+    });
+  };
+
+  $scope.toggleAnimation = function () {
+      $scope.animationsEnabled = !$scope.animationsEnabled;
+  };
+    
+});
+ 
+
+blocChat.controller('ModalCtrl', function ($scope, $uibModalInstance, Room) {
+    $scope.addRoom = function () {     
+        $uibModalInstance.close(); 
+         Room.create($scope.newRoom); 
+    };
+    $scope.cancel = function () {
+        $uibModalInstance.dismiss('cancel');
+    };
+    
 });
